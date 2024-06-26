b5172d204faac0f83eafbc0d4b165cdc5865c4e9
Darrin Johnson <sib47341@adobe.com>
2024-06-26 10:55:04 -0400

    Setup gh-pages initial state

--- ui.frontend/src/components/QueryWizard/FormElements/FormGrid.tsx
+++
@@ -1,9 +0,0 @@
-import {styled} from "@mui/system";
-import Grid from "@mui/material/Grid";
-
-const FormGrid = styled(Grid)(() => ({
-  display: 'flex',
-  flexDirection: 'column',
-}));
-
-export default FormGrid;
