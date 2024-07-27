84f7682fa0a7da69ad70d189c8e02fb47a2f1635
Darrin Johnson <drj101687@gmail.com>
2024-07-27 12:23:49 -0400

    Updated the Github Deployment Job to point to vite site.

--- github3/vite.config.ts
+++ github3/vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/content-wizard/',
   plugins: [tsconfigPaths(), svgr(), react({ tsDecorators: true })],
   test: {
     globals: true,
